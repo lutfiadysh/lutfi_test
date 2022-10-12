6ede7c8b146f699ab875230b60a963b4854bd841
Ariq Heritsa <ariqhm@gmail.com>
2022-10-12 12:49:21 +0700

    add todos

---
+++ lib/data/entities/todo/todo.dart
@@ -0,0 +1,27 @@
+class Todo {
+  Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  int userId;
+  int id;
+  String title;
+  bool completed;
+
+  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+        userId: json["userId"],
+        id: json["id"],
+        title: json["title"],
+        completed: json["completed"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "id": id,
+        "title": title,
+        "completed": completed,
+      };
+}
